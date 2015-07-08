89cf25ed2eab57453abc43ce9cb04d26e216f268
Alfred Francis <alfredfrancis@users.noreply.github.com>
2015-07-09 02:31:54 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,8 +1,12 @@
+<!--
+Authors : Alfred francis,Ajith kumar
+-->
 <html>
   <head>
        <title>Image Similarity Measure </title>
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <meta name="author" content="Alfred francis, Ajith kumar" />
        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
        <style>
             .container {
